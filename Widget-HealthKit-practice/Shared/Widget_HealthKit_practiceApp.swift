592fe2d78fd00f17cda0fa85951b50e3ee2dd826
Sho Emoto <dd2d00seg1@gmail.com>
2022-06-25 16:59:59 +0900

    add project

---
+++ Widget-HealthKit-practice/Shared/Widget_HealthKit_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Widget_HealthKit_practiceApp.swift
+//  Shared
+//
+//  Created by Sho Emoto on 2022/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Widget_HealthKit_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
